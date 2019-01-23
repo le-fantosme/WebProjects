ed917982e270e0989d106a934c434b11045fae9d
le fantosme <le.fantosme@gmail.com>
2019-01-22 20:02:18 -0600

    a generic project upload for demo only.

---
+++ site_monitor_dev.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+import logging
+
+# configure browser and options
+    # driver
+geckodriver = '/home/ou/applications/gecko_driver/geckodriver'
+    # options
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+
+browser = webdriver.Firefox(executable_path=geckodriver, options=options)
+
+# setting up another browser to follow a link
+# TODO: change this..?
+browser_duo = webdriver.Firefox(executable_path=geckodriver, options=options)
+
+# request url
+browser.get('https://finishline.com/store/sneaker-release-dates?mnid=release_calendar')
+# set default logging
+
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+# note : create a formatter
+# create file formatter
+# file_handler.setFormatter('')
+
+'''
+some important notes need to go here
+'''
+
+# handler for logging to file
+# create a handler to log on the filesystem
+file_handler = logging.FileHandler('sneaker_release_finishline.log')
+# file_handler.setFormatter()
+file_handler.setLevel(logging.INFO)
+
+# handler for logging to console
+stream_handler = logging.StreamHandler()
+# stream_handler.setFormatter()
+stream_handler.setLevel(logging.INFO)
+
+# add handlers to root logger
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+for ele in browser.find_elements_by_tag_name('h2'):
+
+    for dateI in browser.find_elements_by_class_name('muted'):
+
+        for dateD in browser.find_elements_by_tag_name('h3'):
+            if "text-left mt-3" in dateD.get_attribute('class'):
+                if ele.text:
+                    logger.info(dateD.text + " " + ele.text + " " + dateI.text)
+
+
+# WebDriverWait(browser,10)
+#print(browser.page_source)
+
+browser.quit()
+
