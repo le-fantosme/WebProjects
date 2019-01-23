c1cb8bfc2fd59d12429ba13324eb38d91540b648
le fantosme <le.fantosme@gmail.com>
2019-01-22 21:45:47 -0600

    shifted some bits removed some unrelated earlier process code

--- site_monitor_dev.py
+++ site_monitor_dev.py
@@ -1,34 +1,15 @@
 from selenium import webdriver
 import logging
 
-# configure browser and options
-    # driver
-geckodriver = '/home/ou/applications/gecko_driver/geckodriver'
-    # options
-options = webdriver.FirefoxOptions()
-options.add_argument('-headless')
+# quick and dirty -
+# set up some default logging
+# use selenium browser automation to request page, parse and log interesting values
 
-browser = webdriver.Firefox(executable_path=geckodriver, options=options)
 
-# setting up another browser to follow a link
-# TODO: change this..?
-browser_duo = webdriver.Firefox(executable_path=geckodriver, options=options)
-
-# request url
-browser.get('https://finishline.com/store/sneaker-release-dates?mnid=release_calendar')
 # set default logging
 
 logger=logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-
-
-# note : create a formatter
-# create file formatter
-# file_handler.setFormatter('')
-
-'''
-some important notes need to go here
-'''
 
 # handler for logging to file
 # create a handler to log on the filesystem
@@ -45,6 +26,19 @@ stream_handler.setLevel(logging.INFO)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
+# configure browser and options
+    # driver
+geckodriver = '/home/ou/applications/gecko_driver/geckodriver'
+    # options
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+
+browser = webdriver.Firefox(executable_path=geckodriver, options=options)
+
+# request url
+browser.get('https://finishline.com/store/sneaker-release-dates?mnid=release_calendar')
+
+# log sneaker release dates
 for ele in browser.find_elements_by_tag_name('h2'):
 
     for dateI in browser.find_elements_by_class_name('muted'):
@@ -53,10 +47,6 @@ for ele in browser.find_elements_by_tag_name('h2'):
             if "text-left mt-3" in dateD.get_attribute('class'):
                 if ele.text:
                     logger.info(dateD.text + " " + ele.text + " " + dateI.text)
-
-
-# WebDriverWait(browser,10)
-#print(browser.page_source)
 
 browser.quit()
 
